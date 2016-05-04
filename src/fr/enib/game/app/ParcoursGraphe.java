a404bb808d119bb0f4ea7301f7f9c277355ad598
ilagam <magali.hautcoeur@hotmail.fr>
2016-05-04 15:14:54 +0200

    Création d'un petit graphe

--- src/fr/enib/game/app/ParcoursGraphe.java
+++ src/fr/enib/game/app/ParcoursGraphe.java
@@ -1,5 +1,10 @@
 package fr.enib.game.app;
 
+import fr.enib.game.model.Lien;
+import fr.enib.game.model.Noeud;
+import fr.enib.game.model.interfaces.ILien;
+import fr.enib.game.model.interfaces.INoeud;
+
 /**
  * @author Ronan Morel
  *
@@ -11,8 +16,19 @@ public class ParcoursGraphe {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		INoeud noeud1 = new Noeud();
+		INoeud noeud2 = new Noeud();
+		
+		noeud1.setId("Patrimoine");
+		noeud2.setId("Architecture");
+		
+		ILien lien1 = new Lien((Noeud)noeud1,(Noeud)noeud2);
+		
+		System.out.println(noeud1.ajouterLienSortant(lien1));
+		System.out.println(noeud2.ajouterLienEntrant(lien1));
+		
+		//System.out.println(noeud1.getId() + " : " + noeud1.getLiensSortant()[0].getId());
+		
 	}
 
 }
